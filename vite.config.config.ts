ef05c385e375268b12110dc5b68665d3d88d551b
Nishida Michiya <m.qrowd@gmail.com>
2026-01-04 08:48:23 +0900

    feat: add Vite configuration files for plugin, desktop, and mobile builds

---
+++ vite.config.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  // Vite configuration options go here
+  build: {
+    target: "esnext",
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/config/main.ts"),
+      plugins: [vue()],
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: undefined,
+        entryFileNames: 'index.js',
+      },
+    },
+    outDir: 'plugin/js/config',
+    emptyOutDir: true,
+  }
+});
---
+++ vite.desktop.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  // Vite configuration options go here
+  build: {
+    target: "esnext",
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/desktop/main.ts"),
+      plugins: [vue()],
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: undefined,
+
+        entryFileNames: 'index.js',
+      },
+    },
+    outDir: 'plugin/js/desktop',
+    emptyOutDir: true,
+  }
+});
