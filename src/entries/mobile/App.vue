7ac25890ebc0b03771336c797a22ffbc36b3d967
Nishida Michiya <m.qrowd@gmail.com>
2026-01-04 08:48:31 +0900

    feat: add initial App.vue and main.ts files for plugin, desktop, and mobile entries

---
+++ src/entries/mobile/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-config">
+    <h2>App Configuration</h2>
+    <p>This is the App configuration page.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
